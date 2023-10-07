f0f7739eb63ac64f40eb8b6d71d71588a1b6a8ef
Francis Poulizac <32905274+fran6p@users.noreply.github.com>
2023-10-07 15:51:30 +0200

    Update plot_graphs.sh

--- Klipper/MyConfiguration/scripts/plot_graphs.sh
+++ Klipper/MyConfiguration/scripts/plot_graphs.sh
@@ -32,7 +32,7 @@
 
 
 #################################################################################################################
-RESULTS_FOLDER=~/klipper_config/adxl_results # Path to the folder where storing the results files
+RESULTS_FOLDER=~/klipper_config/calibrations # Path to the folder where storing the results files
 SCRIPTS_FOLDER=~/klipper_config/scripts # Path to the folder where the graph_vibrations.py is located
 KLIPPER_FOLDER=~/klipper # Path of the klipper main folder
 STORE_RESULTS=3 # Number of results to keep (older files are automatically cleaned). 0 to keep them indefinitely
