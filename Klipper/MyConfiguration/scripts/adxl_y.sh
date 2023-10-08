4a9b422b994c438464978dfaf291e87e241ea37d
Francis Poulizac <32905274+fran6p@users.noreply.github.com>
2023-10-08 16:54:31 +0200

    Update adxl_y.sh

--- Klipper/MyConfiguration/scripts/adxl_y.sh
+++ Klipper/MyConfiguration/scripts/adxl_y.sh
@@ -8,6 +8,6 @@
 DATE=$(date +"%Y%m%d")
 SCRIPTS="~/klipper/scripts/calibrate_shaper.py"
 CSV_FILE="/tmp/calibration_data_y_*.csv"
-PNG_FILE="~/klipper_config/calibrations/shaper_calibrate_y_$DATE.png"
+PNG_FILE="~/klipper_config/adxl_results/shaper_calibrate_y_$DATE.png"
 
 python3 $SCRIPTS $CSV_FILE -o $PNG_FILE
